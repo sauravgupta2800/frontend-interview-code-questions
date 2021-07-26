a2bbfb31c87c82d4c0630ff568143ec04fb04bc2
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-07-26 13:13:15 +0530

    protypal inheritance conclusion added

--- prototype-and-prototypal-inheritance.js
+++ prototype-and-prototypal-inheritance.js
@@ -43,3 +43,9 @@ object2.info(); // It has acces of info method of object. It comes through inher
 Function.prototype.myBind = function () {
   console.log("I am my Bind .");
 };
+
+// Conclusion : https://codeburst.io/javascript-under-the-hood-pt-5-prototype-and-prototypal-inheritance-e5c400dba41d
+// Here’s what we’ve covered in this article:
+// Every object has something called a “prototype”. A prototype is an object connected to an object that allows it to access properties and methods from another, related object.
+// Prototypal inheritance is allowing an object to access the methods and properties of another object by using prototypes.
+// The JavaScript engine uses something called the “prototype” chain to access properties and methods on objects. If we try to access a property or method on an object that doesn’t directly have the property or method, the JavaScript engine will look at the prototype of each object, going all the way down the chain, until it finds it or doesn’t.
