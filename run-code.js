43dc1918a7aacd7343ab0fb5ed613b70c9bb409b
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-07-28 18:04:45 +0530

    minimu swap to sort

--- run-code.js
+++ run-code.js
@@ -1,34 +1,14 @@
-function solve(intervals, new_interval) {
-  const arr = [...intervals];
-  new_interval.sort((a, b) => a - b);
-  arr.push(new_interval);
-  arr.sort((a, b) => a[0] - b[0]);
-  if (arr.length <= 1) return arr;
-
-  let res = [];
-  let currStart = arr[0][0];
-  let currEnd = arr[0][1];
-
-  for (let i = 1; i < arr.length; i++) {
-    const [start, end] = arr[i];
-    if (start > currEnd) {
-      res.push([currStart, currEnd]);
-      currStart = start;
-      currEnd = end;
-    } else {
-      currEnd = Math.max(currEnd, end);
+function solve(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i] !== i) {
+      count++;
+      const temp = arr[arr[i]];
+      arr[arr[i]] = arr[i];
+      arr[i] = temp;
     }
   }
-  res.push([currStart, currEnd]);
-  return res;
+  return count;
 }
 
-console.log(
-  solve(
-    [
-      [2, 3],
-      [6, 9],
-    ],
-    [1, 60]
-  )
-);
+console.log(solve([1, 2, 3, 4, 0]));
